9e9fde97a19be71c1dcfc4c624d204af50c11c54
arronh12 <arronhoare@gmail.com>
2020-09-20 05:23:26 +0100

    second commit - added functionality for login and logout. starting to build db of users.
    features:
    - User db
    - flash messages for user feedback
    - added bootstrap on forms

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "45Q@3DS*wo64Uzae%98hmargh"
@@ -17,25 +18,43 @@ def login():
         session.permanent = True
         current_usr = request.form['nm']
         session['user'] = current_usr
-
+        flash("Login successful!...")
         return redirect(url_for("user"))
     else:
+        if "user" in session:
+            flash("Already logged in!...")
+            return redirect(url_for("user"))
+
         return render_template('login.html')
 
 
 @app.route("/logout")
 def logout():
+    if 'user' in session:
+        usr = session['user']
+        flash(f"You were logged out, {usr}", "info")
     session.pop("user", None)
-    flash('You were logged out...', "info")
+    session.pop("email", None)
     return redirect(url_for("login"))
 
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         usr = session["user"]
-        return f"<h1>{usr}</h1>"
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            flash("Email was saved!...")
+        else:
+            if "email" in session:
+                email = session["email"]
+
+        return render_template('user.html', email=email)
     else:
+        flash("You are not logged in!...")
         return redirect(url_for('login'))
 
 
