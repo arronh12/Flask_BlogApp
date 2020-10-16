a68608e0fe201b427eb9d37c6d954c9b914b41bf
arronh12 <arronhoare@gmail.com>
2020-10-16 23:03:52 +0100

    third commit - basic SQLalchemy functionality added for user database.
    features:
    - SQLalchemy functionality.
    - persistent user data saved both to session and SQLalchemy db.

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
 app.secret_key = "45Q@3DS*wo64Uzae%98hmargh"
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 
 @app.route("/")
@@ -18,6 +34,16 @@ def login():
         session.permanent = True
         current_usr = request.form['nm']
         session['user'] = current_usr
+
+        found_usr = db.session.query(User).filter_by(name=current_usr).first()
+
+        if found_usr:
+            session['email'] = found_usr.email
+        else:
+            usr = User(current_usr, "")
+            db.session.add(usr)
+            db.session.commit()
+
         flash("Login successful!...")
         return redirect(url_for("user"))
     else:
@@ -42,11 +68,14 @@ def logout():
 def user():
     email = None
     if "user" in session:
-        usr = session["user"]
+        current_usr = session["user"]
 
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            found_usr = db.session.query(User).filter_by(name=current_usr).first()
+            found_usr.email = email
+            db.session.commit()
             flash("Email was saved!...")
         else:
             if "email" in session:
@@ -59,4 +88,5 @@ def user():
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
