83bc9da0e6c19f51c4bd86bc6649d63843ad03e3
arronh12 <arronhoare@gmail.com>
2020-10-29 02:32:30 +0000

    5th commit

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 from datetime import date, datetime
+from passlib.hash import sha256_crypt
+
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = '45Q@3DS*wo64Uzae%98hmargh'
 
 db = SQLAlchemy(app)
 
@@ -21,11 +24,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
+    password = db.Column(db.String(300))
     posts = db.relationship('Post', backref='user', lazy='dynamic')
 
-    def __init__(self, name, email):
+    def __init__(self, name, email, password):
         self.name = name
         self.email = email
+        self.password = password
 
     def __repr__(self):
         return self.name
@@ -51,15 +56,20 @@ def home():
 def login():
     if request.method == "POST":
         session.permanent = True
-        current_usr = request.form['nm']
+        current_usr = request.form['login_name']
+        current_pw = request.form['login_pw']
+
+        hashed_pw = sha256_crypt.encrypt(current_pw)
+
         session['user'] = current_usr
+        print("User:" + current_usr + ", Password:" + current_pw)
 
         found_usr = db.session.query(User).filter_by(name=current_usr).first()
 
         if found_usr:
             session['email'] = found_usr.email
         else:
-            usr = User(current_usr, "")
+            usr = User(current_usr, "", hashed_pw)
             db.session.add(usr)
             db.session.commit()
 
@@ -81,6 +91,34 @@ def logout():
     session.pop("user", None)
     session.pop("email", None)
     return redirect(url_for("login"))
+
+
+@app.route("/reg_user", methods=["POST", "GET"])
+def reg_user():
+    if request.method == "POST":
+        usr_name = request.form['reg_name']
+        usr_email = request.form['reg_email']
+        pw_1 = request.form['reg_pw_1']
+        pw_2 = request.form['reg_pw_2']
+
+        if pw_1 == pw_2:
+            hashed_pw = sha256_crypt.encrypt(pw_1)
+            make_usr = User(name=usr_name, email=usr_email, password=hashed_pw)
+            db.session.add(make_usr)
+            db.session.commit()
+            session['user'] = usr_name
+            session['email'] = usr_email
+            flash("You are registered successfully....")
+            return redirect(url_for("user"))
+        else:
+            flash("Your passwords do not match!....")
+            return redirect(url_for("reg_user"))
+    else:
+        if "user" in session:
+            flash("Already logged in!....")
+            return redirect(url_for("user"))
+
+        return render_template("reg_user.html")
 
 
 @app.route("/friends")
@@ -127,6 +165,20 @@ def posts():
     else:
         flash("you are not logged in...")
         return render_template("posts.html")
+
+
+@app.route("/make_post", methods=["POST", "GET"])
+def make_post():
+    if request.method == "POST":
+        p_title = request.form['postTitleInput']
+        p_content = request.form['postContentInput']
+        usr = db.session.query(User).filter_by(name=session['user']).first()
+        made_post = Post(post_title=p_title, post_content=p_content, user_id=usr.id)
+        db.session.add(made_post)
+        db.session.commit()
+        return redirect(url_for("posts"))
+    else:
+        return render_template('make_post.html')
 
 
 if __name__ == '__main__':
